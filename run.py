2585bd2b7906266f8d317eb5e73e337aa78a131c
dbstj1231 <80622014+dbstj1231@users.noreply.github.com>
2022-09-29 14:02:46 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 
-import drone_asr as ASR
+import drone_asr as asr
 
 
+# DRONE_WAV_PATH = "/home/speechlab/drone_asr/speech.wav"
 DRONE_WAV_PATH = "/home/yoonseo/drone_asr/speech.wav"
 
 
@@ -10,24 +11,28 @@ if __name__ =="__main__":
 
     #initializeing
     print("Model initializing...")
-    ASR = ASR.Drone_ASR()
+    ASR = asr.Drone_ASR()
     print("Model loaded completed.")
     
 
     while True:
         key = input()
         if key == 'S' or key == 's':
+
             print("Recording...")
+            # recording 5 second wav file
             os.system("arecord -t wav -c 1 -D plughw:2,0 -f S16_LE -d 6 -r 16000 " + DRONE_WAV_PATH)
             print("Recording finished.")
     
+            print("Transcribe...")
+            #transcribe
             file_list_wav = [DRONE_WAV_PATH]
             result = ASR.drone_transcribe(file_list_wav)
-            
-            if ASR.check_valid_cmd(result):
-                print("True")
-            else:
-                print("False")
+            print("Done!!!")
+
+            #print out the result
             print(result)
+            
+            
         else :
             break
